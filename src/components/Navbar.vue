7b66fd31db47506e49c6ee3ef64ff22e6986d740
ZazaGdev <Gejadze250@gmail.com>
2023-09-22 00:20:42 +0400

    Implemented navbar and logout functionality

---
+++ src/components/Navbar.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="navbar">
+        <nav>
+            <h1>
+                <router-link :to="{ name: 'Home' }">Movie<span>X</span></router-link>
+            </h1>
+
+            <div class="links">
+                <div v-if="user">
+                    <button @click="handleLogout">Logout</button>
+                </div>
+                <div v-else>
+                    <router-link v-if="!user" class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+                    <router-link v-if="!user" class="btn" :to="{ name: 'Login' }">Login</router-link>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+import useLogout from '@/composables/useLogout'
+import { useRouter } from 'vue-router'
+import getUser from '@/composables/getUser'
+
+export default {
+    setup() {
+        const { logout } = useLogout()
+        const router = useRouter()
+        const { user } = getUser()
+        console.log(user.value)
+
+        const handleLogout = async () => {
+            await logout()
+
+            console.log('User Logged Out')
+            router.push({ name: 'Login' })
+        }
+
+        return { handleLogout, user }
+    },
+}
+</script>
+
+<style scoped>
+.navbar {
+    padding: 16px 10px;
+    margin-bottom: 60px;
+    background: white;
+}
+nav {
+    display: flex;
+    align-items: center;
+    max-width: 1200px;
+    margin: 0 auto;
+}
+nav img {
+    max-height: 60px;
+}
+nav h1 {
+    margin-left: 20px;
+}
+nav h1 a {
+    display: flex;
+    align-items: center;
+}
+nav h1 span {
+    font-size: 2rem;
+}
+nav .links {
+    margin-left: auto;
+}
+nav .links a,
+button {
+    margin-left: 16px;
+    font-size: 14px;
+}
+</style>
---
+++ src/views/auth/Signup.vue
@@ -0,0 +1,37 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <h3>Signup</h3>
+        <input type="text" placeholder="Display Name" v-model="displayName" required />
+        <input type="text" placeholder="Email" v-model="email" required />
+        <input type="password" placeholder="Password" v-model="password" required />
+        <div class="error" v-if="error">{{ error }}</div>
+        <button v-if="!isPending">Sign up</button>
+        <button v-if="isPending" disabled>Loading</button>
+    </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+import useSignup from '@/composables/useSignup'
+
+export default {
+    setup() {
+        const { error, signup, isPending } = useSignup()
+
+        const handleSubmit = async () => {
+            const res = await signup(email.value, password.value, displayName.value)
+            if (!error.value) {
+                console.log('User signed up')
+            }
+        }
+
+        const email = ref('')
+        const password = ref('')
+        const displayName = ref('')
+
+        return { email, password, displayName, isPending, error, handleSubmit }
+    },
+}
+</script>
+
+<style></style>
