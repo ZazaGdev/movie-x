c14d61d22e993eb65dfb1b5ed58f648c9300f039
ZazaGdev <Gejadze250@gmail.com>
2023-09-26 21:09:59 +0400

    Added query for user collections page

--- src/views/Collections/UserCollections.vue
+++ src/views/Collections/UserCollections.vue
@@ -1,8 +1,23 @@
 <template>
-    <div class="user-collections">My Collections</div>
+    <div class="user-collections">
+        <h2>My Playlists</h2>
+        <div v-if="collections">
+            <CollectionsList :collections="collections" />
+        </div>
+        <router-link class="btn" :to="{ name: 'CreateCollection' }">Create New Playlist</router-link>
+    </div>
 </template>
 <script>
+import CollectionsList from '@/components/CollectionsList.vue'
+import getCollection from '@/composables/getCollection'
+import getUser from '@/composables/getUser'
+
 export default {
-    setup() {},
+    setup() {
+        const { user } = getUser()
+        const { documents: collections } = getCollection('collections', ['userId', '==', user.value.uid])
+        return { collections }
+    },
+    components: { CollectionsList },
 }
 </script>
